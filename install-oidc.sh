31e0120c1d302ea83312b246b1e25394c891f87c
reggi <reggi@github.com>
2025-10-01 11:05:19 -0400

    get token

--- install-oidc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-TMP_DIR=$(mktemp -d)
-git clone --depth 1 --single-branch --branch oidc https://github.com/npm/cli.git "$TMP_DIR/cli"
-cd "$TMP_DIR/cli"
-npm install
-npm install -g .
-cd -
-npm config set loglevel silly
